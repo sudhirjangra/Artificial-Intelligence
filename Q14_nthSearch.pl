c69a144d2a2ac1623e4a73223105e035fe0e9580
sudhirjangra <sachinjangra@Sachins-Air.lan>
2024-03-12 22:08:04 +0530

    List_1

--- Q14_nthSearch.pl
+++ Q14_nthSearch.pl
@@ -1,11 +1,12 @@
-main1:-
-    write('Enter the list: '),
-    read(List),
-    write('Enter the element to search: '),
-    read(Number),
-    search(Number, List),
-    write('Element Found!');
-    write('Element not Found!').
-search(Number,[]):-false.
-search(Number,[Number|_]).
-search(Number,[Y|Tail]):-search(Number,Tail).
+main:-
+    write('Enter the list : '),
+    read(L),
+    write('Enter the position you want to search for : '),
+    read(N),
+    nth_element(L,N,X),
+    write(X).
+   nth_element([H|T],1,H).
+   nth_element([H|Tail],N,X):- 
+       M is N-1,
+       nth_element(Tail,M,B),
+       X is B.
